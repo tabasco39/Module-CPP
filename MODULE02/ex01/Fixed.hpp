d038f5a8f56aa8be24f2480f0894c80a7a0f2e2f
Antsa <ranaivoantsaa@gmail.com>
2025-01-06 21:30:14 +0300

    member function implementation (MOD02 - ex01 )

---
+++ MODULE02/ex01/Fixed.hpp
@@ -0,0 +1,26 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+class Fixed
+{
+private:
+    int                 fixed_value;
+    static const int    bit = 8;
+public:
+    Fixed(void);
+    Fixed(const Fixed& obj);
+    Fixed(const int nb);
+    Fixed(const float nb);
+    ~Fixed();
+    float   toFloat(void) const;
+    int     toInt(void) const;
+    Fixed & operator=(Fixed const &obj);
+    friend std::ostream&   operator<<(std::ostream& os, Fixed const &obj);
+    int     getRawBits(void) const;
+    void    setRawBits(int const raw);
+};
+
+
+#endif
